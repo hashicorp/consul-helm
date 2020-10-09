3283b50c0abb7de28c5bd03fce7629edb536a563
Kyle Schochenmaier <kschoche@gmail.com>
2020-10-09 12:13:31 -0500

    start test with passing probes and add autoencrypt and tls tests

--- test/acceptance/tests/connect/health_checks_test.go
+++ test/acceptance/tests/connect/health_checks_test.go
@@ -1,8 +1,10 @@
 package connect
 
 import (
+	"fmt"
 	"github.com/hashicorp/consul-helm/test/acceptance/framework"
 	"github.com/hashicorp/consul-helm/test/acceptance/helpers"
+	"strconv"
 	"testing"
 )
 
@@ -16,32 +18,58 @@ import (
 // Test that traffic fails
 // kubectl exec to the static-client and `touch /tmp/healthy`
 // Test that traffic passes
-func TestHealthChecksDefault(t *testing.T) {
-	cfg := suite.Config()
-	ctx := suite.Environment().DefaultContext(t)
-
-	helmValues := map[string]string{
-		"global.imageK8S":                    "kschoche/consul-k8s-dev",
-		"connectInject.enabled":              "true",
-		"connectInject.healthChecks.enabled": "true",
-		"global.tls.enabled":                 "true",
+func TestHealthChecksSecure(t *testing.T) {
+	cases := []struct {
+		secure      bool
+		autoEncrypt bool
+	}{
+		{
+			false,
+			false,
+		},
+		{
+			true,
+			true,
+		},
+		{
+			true,
+			true,
+		},
 	}
 
-	releaseName := helpers.RandomName()
-	consulCluster := framework.NewHelmCluster(t, helmValues, ctx, cfg, releaseName)
-	consulCluster.Create(t)
+	for _, c := range cases {
+		name := fmt.Sprintf("secure: %t, auto-encrypt: %t", c.secure, c.autoEncrypt)
+		t.Run(name, func(t *testing.T) {
+			ctx := suite.Environment().DefaultContext(t)
+			cfg := suite.Config()
 
-	t.Log("creating static-server and static-client deployments")
-	helpers.DeployKustomizeWithoutWait(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.DebugDirectory, "../fixtures/cases/static-server-hc")
-	helpers.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.DebugDirectory, "../fixtures/cases/static-client-inject")
-	// The readiness probe takes 2s to report success
-	t.Log("checking that connection is unsuccessful")
-	helpers.CheckStaticServerConnection(t, ctx.KubectlOptions(t), false, staticClientName, "http://localhost:1234")
+			helmValues := map[string]string{
+				"global.imageK8S":                    "kschoche/consul-k8s-dev",
+				"connectInject.enabled":              "true",
+				"connectInject.healthChecks.enabled": "true",
 
-	// Now remove the file so that the health check fails
-	helpers.RunKubectl(t, ctx.KubectlOptions(t), "exec", "-it", "deploy/"+staticServerName, "--", "touch", "/tmp/healthy")
+				// TODO: When ACLs are supported add another test
+				//"global.acls.manageSystemACLs": strconv.FormatBool(c.secure),
+				"global.tls.enabled":     strconv.FormatBool(c.secure),
+				"global.tls.autoEncrypt": strconv.FormatBool(c.autoEncrypt),
+			}
 
-	// The readiness probe should take a few seconds to populate consul, CheckStaticServerConnection retries until it fails
-	t.Log("checking that connection is successful")
-	helpers.CheckStaticServerConnection(t, ctx.KubectlOptions(t), true, staticClientName, "http://localhost:1234")
+			releaseName := helpers.RandomName()
+			consulCluster := framework.NewHelmCluster(t, helmValues, ctx, cfg, releaseName)
+			consulCluster.Create(t)
+
+			t.Log("creating static-server and static-client deployments")
+			helpers.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.DebugDirectory, "../fixtures/cases/static-server-hc")
+			helpers.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.DebugDirectory, "../fixtures/cases/static-client-inject")
+			t.Log("checking that connection is successful")
+			helpers.CheckStaticServerConnection(t, ctx.KubectlOptions(t), true, staticClientName, "http://localhost:1234")
+
+			// Now remove the file so that the health check fails
+			helpers.RunKubectl(t, ctx.KubectlOptions(t), "exec", "-it", "deploy/"+staticServerName, "--", "touch", "/tmp/unhealthy")
+
+			// The readiness probe should take a few seconds to populate consul, CheckStaticServerConnection retries until it fails
+			t.Log("checking that connection is unsuccessful")
+			helpers.CheckStaticServerConnection(t, ctx.KubectlOptions(t), false, staticClientName, "http://localhost:1234")
+		})
+	}
 }
