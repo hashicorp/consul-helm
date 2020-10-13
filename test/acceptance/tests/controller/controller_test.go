9d2a0a541fbbc9417b3e62759f0dd08a9008adb5
Luke Kysow <1034429+lkysow@users.noreply.github.com>
2020-10-13 16:58:29 -0700

    Test controller against consul:1.9.0-beta1

--- test/acceptance/tests/controller/controller_test.go
+++ test/acceptance/tests/controller/controller_test.go
@@ -38,8 +38,7 @@ func TestController(t *testing.T) {
 			helmValues := map[string]string{
 				"controller.enabled":    "true",
 				"connectInject.enabled": "true",
-				// todo: remove when 1.9.0 is released.
-				"global.image": "hashicorpdev/consul",
+				"global.image":          "consul:1.9.0-beta1",
 
 				"global.tls.enabled":           strconv.FormatBool(c.secure),
 				"global.tls.enableAutoEncrypt": strconv.FormatBool(c.autoEncrypt),
