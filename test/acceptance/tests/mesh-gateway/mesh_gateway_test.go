ea49711581a6b451ab96c7cf65f5b60382fc4310
Iryna Shustava <iryna@hashicorp.com>
2020-11-25 07:48:21 -1000

    Add a clarification for why we're using health API to verify federation

--- test/acceptance/tests/mesh-gateway/mesh_gateway_test.go
+++ test/acceptance/tests/mesh-gateway/mesh_gateway_test.go
@@ -228,6 +228,14 @@ func TestMeshGatewaySecure(t *testing.T) {
 func verifyFederation(t *testing.T, primaryClient, secondaryClient *api.Client, releaseName string, secure bool) {
 	retrier := &retry.Timer{Timeout: 2 * time.Minute, Wait: 1 * time.Second}
 
+	// Check that server in dc1 is healthy from the perspective of the server in dc2, and vice versa.
+	// We're calling the Consul health API, as opposed to checking serf membership status,
+	// because we need to make sure that the federated servers can make API calls and forward requests
+	// from one server to another. From running tests in CI for a while and using serf membership status before,
+	// we've noticed that the status could be "alive" as soon as the server in the secondary cluster joins the primary
+	// and then switch to "failed". This would require us to check that the status is "alive" is showing consistently for
+	// some amount of time, which could be quite flakey. Calling the API in another datacenter allows us to check that
+	// each server can forward calls to another, which is what we need for connect.
 	retry.RunWith(retrier, t, func(r *retry.R) {
 		secondaryServerHealth, _, err := primaryClient.Health().Node(fmt.Sprintf("%s-consul-server-0", releaseName), &api.QueryOptions{Datacenter: "dc2"})
 		require.NoError(r, err)
