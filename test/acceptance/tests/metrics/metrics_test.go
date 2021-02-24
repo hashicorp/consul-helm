f1e440c6de4ca340a22e8bba45046fb27c4e4ca5
Iryna Shustava <iryna@hashicorp.com>
2021-02-24 18:13:15 -0500

    Update tests based on review from Iryna Shustava (iryna@hashicorp.com)

--- test/acceptance/tests/metrics/metrics_test.go
+++ test/acceptance/tests/metrics/metrics_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/consul-helm/test/acceptance/framework/consul"
+	"github.com/hashicorp/consul-helm/test/acceptance/framework/environment"
 	"github.com/hashicorp/consul-helm/test/acceptance/framework/helpers"
 	"github.com/hashicorp/consul-helm/test/acceptance/framework/k8s"
 	"github.com/hashicorp/consul-helm/test/acceptance/framework/logger"
@@ -15,13 +16,13 @@ import (
 
 const staticClientName = "static-client"
 
-// Test that Connect and wan federation over mesh gateways work in a default installation
-// i.e. without ACLs because TLS is required for WAN federation over mesh gateways
+// Test that prometheus metrics, when enabled, are accessible from the
+// endpoints that have been exposed on the server, client and gateways.
 func TestMetrics(t *testing.T) {
 	env := suite.Environment()
 	cfg := suite.Config()
-
 	ctx := env.DefaultContext(t)
+	ns := ctx.KubectlOptions(t).Namespace
 
 	helmValues := map[string]string{
 		"global.datacenter":                 "dc1",
@@ -46,72 +47,47 @@ func TestMetrics(t *testing.T) {
 	if cfg.UseKind {
 		helmValues["meshGateway.service.type"] = "NodePort"
 		helmValues["meshGateway.service.nodePort"] = "30000"
-		helmValues["ingressGateways.service.type"] = "NodePort"
-		helmValues["ingressGateways.service.nodePort"] = "31000"
-		helmValues["terminatingGateways.service.type"] = "NodePort"
-		helmValues["terminatingGateways.service.nodePort"] = "32000"
 	}
 
 	releaseName := helpers.RandomName()
 
-	// Install the consul cluster in the default kubernetes ctx
+	// Install the consul cluster in the default kubernetes ctx.
 	consulCluster := consul.NewHelmCluster(t, helmValues, ctx, cfg, releaseName)
 	consulCluster.Create(t)
 
-	_ = consulCluster.SetupConsulClient(t, false)
-
+	// Create the static-client deployment so we can use it for in-cluster calls to metrics endpoints.
+	// This simulates queries that would be made by a prometheus server that runs externally to the consul
+	// components in the cluster.
 	logger.Log(t, "creating static-client")
 	k8s.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.DebugDirectory, "../fixtures/cases/static-client-inject")
 
 	// Server Metrics
-	ns := ctx.KubectlOptions(t).Namespace
-	servers, err := ctx.KubernetesClient(t).CoreV1().Pods(ns).List(context.Background(), metav1.ListOptions{LabelSelector: "component=server"})
+	metricsOutput, err := k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "exec", "deploy/"+staticClientName, "--", "curl", fmt.Sprintf("http://%s:8500/v1/agent/metrics?format=prometheus", fmt.Sprintf("%s-consul-server.%s.svc", releaseName, ns)))
 	require.NoError(t, err)
-	for _, pod := range servers.Items {
-		podIP := pod.Status.PodIP
-		metricsOutput, err := k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "exec", "deploy/"+staticClientName, "--", "curl", fmt.Sprintf("http://%s:8500/v1/agent/metrics?format=prometheus", podIP))
-		require.NoError(t, err)
-		require.Contains(t, metricsOutput, `consul_acl_ResolveToken{quantile="0.5"}`)
-	}
+	require.Contains(t, metricsOutput, `consul_acl_ResolveToken{quantile="0.5"}`)
 
 	// Client Metrics
-	clients, err := ctx.KubernetesClient(t).CoreV1().Pods(ns).List(context.Background(), metav1.ListOptions{LabelSelector: "component=client"})
+	metricsOutput, err = k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "exec", "deploy/"+staticClientName, "--", "sh", "-c", "curl http://$HOST_IP:8500/v1/agent/metrics?format=prometheus")
 	require.NoError(t, err)
-	for _, pod := range clients.Items {
-		podIP := pod.Status.PodIP
-		metricsOutput, err := k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "exec", "deploy/"+staticClientName, "--", "curl", fmt.Sprintf("http://%s:8500/v1/agent/metrics?format=prometheus", podIP))
-		require.NoError(t, err)
-		require.Contains(t, metricsOutput, `consul_acl_ResolveToken{quantile="0.5"}`)
-	}
+	require.Contains(t, metricsOutput, `consul_acl_ResolveToken{quantile="0.5"}`)
 
 	// Ingress Gateway Metrics
-	igGateways, err := ctx.KubernetesClient(t).CoreV1().Pods(ns).List(context.Background(), metav1.ListOptions{LabelSelector: "component=ingress-gateway"})
-	require.NoError(t, err)
-	for _, pod := range igGateways.Items {
-		podIP := pod.Status.PodIP
-		metricsOutput, err := k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "exec", "deploy/"+staticClientName, "--", "curl", fmt.Sprintf("http://%s:20200/metrics", podIP))
-		require.NoError(t, err)
-		require.Contains(t, metricsOutput, `envoy_cluster_assignment_stale{local_cluster="ingress-gateway",consul_source_service="ingress-gateway",consul_source_namespace="default",consul_source_datacenter="dc1",envoy_cluster_name="local_agent"} 0`)
-	}
+	assertGatewayMetricsEnabled(t, ctx, ns, "ingress-gateway", `envoy_cluster_assignment_stale{local_cluster="ingress-gateway",consul_source_service="ingress-gateway",consul_source_namespace="default",consul_source_datacenter="dc1",envoy_cluster_name="local_agent"} 0`)
 
 	// Terminating Gateway Metrics
-	termGateways, err := ctx.KubernetesClient(t).CoreV1().Pods(ns).List(context.Background(), metav1.ListOptions{LabelSelector: "component=terminating-gateway"})
-	require.NoError(t, err)
-	for _, pod := range termGateways.Items {
-		podIP := pod.Status.PodIP
-		metricsOutput, err := k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "exec", "deploy/"+staticClientName, "--", "curl", fmt.Sprintf("http://%s:20200/metrics", podIP))
-		require.NoError(t, err)
-		require.Contains(t, metricsOutput, `envoy_cluster_assignment_stale{local_cluster="terminating-gateway",consul_source_service="terminating-gateway",consul_source_namespace="default",consul_source_datacenter="dc1",envoy_cluster_name="local_agent"} 0`)
-	}
+	assertGatewayMetricsEnabled(t, ctx, ns, "terminating-gateway", `envoy_cluster_assignment_stale{local_cluster="terminating-gateway",consul_source_service="terminating-gateway",consul_source_namespace="default",consul_source_datacenter="dc1",envoy_cluster_name="local_agent"} 0`)
 
 	// Mesh Gateway Metrics
-	meshGateways, err := ctx.KubernetesClient(t).CoreV1().Pods(ns).List(context.Background(), metav1.ListOptions{LabelSelector: "component=mesh-gateway"})
+	assertGatewayMetricsEnabled(t, ctx, ns, "mesh-gateway", `envoy_cluster_assignment_stale{local_cluster="mesh-gateway",consul_source_service="mesh-gateway",consul_source_namespace="default",consul_source_datacenter="dc1",envoy_cluster_name="local_agent"} 0`)
+}
+
+func assertGatewayMetricsEnabled(t *testing.T, ctx environment.TestContext, ns, label, metricsAssertion string) {
+	pods, err := ctx.KubernetesClient(t).CoreV1().Pods(ns).List(context.Background(), metav1.ListOptions{LabelSelector: fmt.Sprintf("component=%s", label)})
 	require.NoError(t, err)
-	for _, pod := range meshGateways.Items {
+	for _, pod := range pods.Items {
 		podIP := pod.Status.PodIP
 		metricsOutput, err := k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "exec", "deploy/"+staticClientName, "--", "curl", fmt.Sprintf("http://%s:20200/metrics", podIP))
 		require.NoError(t, err)
-		require.Contains(t, metricsOutput, `envoy_cluster_assignment_stale{local_cluster="mesh-gateway",consul_source_service="mesh-gateway",consul_source_namespace="default",consul_source_datacenter="dc1",envoy_cluster_name="local_agent"} 0`)
+		require.Contains(t, metricsOutput, metricsAssertion)
 	}
-
 }
