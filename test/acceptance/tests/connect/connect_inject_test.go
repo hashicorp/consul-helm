57cec48577376185dcb0d7aa04905701c86edca8
Iryna Shustava <iryna@hashicorp.com>
2021-04-09 09:48:24 -0600

    Support transparent proxy in the consul helm chart
    * Add new connectInject.transparentProxy.defaultEnabled value (default to true)
      that will allow users to enable or disable tproxy for each helm installation.
    * Add acceptance tests for connect-inject to test with tproxy

--- test/acceptance/tests/connect/connect_inject_test.go
+++ test/acceptance/tests/connect/connect_inject_test.go
@@ -31,65 +31,100 @@ func TestConnectInject(t *testing.T) {
 		{true, true},
 	}
 
+	tproxyEnabledCases := []bool{false, true}
+
 	for _, c := range cases {
-		name := fmt.Sprintf("secure: %t; auto-encrypt: %t", c.secure, c.autoEncrypt)
-		t.Run(name, func(t *testing.T) {
-			cfg := suite.Config()
-			ctx := suite.Environment().DefaultContext(t)
+		for _, tproxyEnabled := range tproxyEnabledCases {
+			name := fmt.Sprintf("secure: %t; auto-encrypt: %t; tproxy: %t", c.secure, c.autoEncrypt, tproxyEnabled)
+			t.Run(name, func(t *testing.T) {
+				cfg := suite.Config()
+				ctx := suite.Environment().DefaultContext(t)
 
-			helmValues := map[string]string{
-				"connectInject.enabled":        "true",
-				"global.tls.enabled":           strconv.FormatBool(c.secure),
-				"global.tls.enableAutoEncrypt": strconv.FormatBool(c.autoEncrypt),
-				"global.acls.manageSystemACLs": strconv.FormatBool(c.secure),
-			}
+				// todo: set images globally rather than per test
+				helmValues := map[string]string{
+					"global.image":    "ishustava/consul-dev:tproxy-test",
+					"global.imageK8S": "ishustava/consul-k8s-dev:04-13-2021-65883b7",
 
-			releaseName := helpers.RandomName()
-			consulCluster := consul.NewHelmCluster(t, helmValues, ctx, cfg, releaseName)
+					"connectInject.enabled":                         "true",
+					"connectInject.transparentProxy.defaultEnabled": strconv.FormatBool(tproxyEnabled),
 
-			consulCluster.Create(t)
+					"global.tls.enabled":           strconv.FormatBool(c.secure),
+					"global.tls.enableAutoEncrypt": strconv.FormatBool(c.autoEncrypt),
+					"global.acls.manageSystemACLs": strconv.FormatBool(c.secure),
+				}
 
-			logger.Log(t, "creating static-server and static-client deployments")
-			k8s.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.DebugDirectory, "../fixtures/cases/static-server-inject")
-			k8s.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.DebugDirectory, "../fixtures/cases/static-client-inject")
+				releaseName := helpers.RandomName()
+				consulCluster := consul.NewHelmCluster(t, helmValues, ctx, cfg, releaseName)
 
-			if c.secure {
-				logger.Log(t, "checking that the connection is not successful because there's no intention")
-				k8s.CheckStaticServerConnectionFailing(t, ctx.KubectlOptions(t), staticClientName, "http://localhost:1234")
+				consulCluster.Create(t)
 
-				consulClient := consulCluster.SetupConsulClient(t, true)
+				logger.Log(t, "creating static-server and static-client deployments")
+				// todo: check that injection is successful
+				k8s.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.DebugDirectory, "../fixtures/cases/static-server-inject")
+				if tproxyEnabled {
+					k8s.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.DebugDirectory, "../fixtures/cases/static-client-tproxy")
+				} else {
+					k8s.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.DebugDirectory, "../fixtures/cases/static-client-inject")
+				}
 
-				logger.Log(t, "creating intention")
-				_, _, err := consulClient.Connect().IntentionCreate(&api.Intention{
-					SourceName:      staticClientName,
-					DestinationName: staticServerName,
-					Action:          api.IntentionActionAllow,
-				}, nil)
-				require.NoError(t, err)
-			}
+				if c.secure {
+					logger.Log(t, "checking that the connection is not successful because there's no intention")
+					if tproxyEnabled {
+						// todo: check that traffic is going through the proxy
+						k8s.CheckStaticServerConnectionFailing(t, ctx.KubectlOptions(t), staticClientName, "http://static-server")
+					} else {
+						k8s.CheckStaticServerConnectionFailing(t, ctx.KubectlOptions(t), staticClientName, "http://localhost:1234")
+					}
 
-			logger.Log(t, "checking that connection is successful")
-			k8s.CheckStaticServerConnectionSuccessful(t, ctx.KubectlOptions(t), staticClientName, "http://localhost:1234")
+					consulClient := consulCluster.SetupConsulClient(t, true)
 
-			// Test that kubernetes readiness status is synced to Consul.
-			// Create the file so that the readiness probe of the static-server pod fails.
-			logger.Log(t, "testing k8s -> consul health checks sync by making the static-server unhealthy")
-			k8s.RunKubectl(t, ctx.KubectlOptions(t), "exec", "deploy/"+staticServerName, "--", "touch", "/tmp/unhealthy")
+					logger.Log(t, "creating intention")
+					_, _, err := consulClient.Connect().IntentionCreate(&api.Intention{
+						SourceName:      staticClientName,
+						DestinationName: staticServerName,
+						Action:          api.IntentionActionAllow,
+					}, nil)
+					require.NoError(t, err)
+				}
 
-			// The readiness probe should take a moment to be reflected in Consul, CheckStaticServerConnection will retry
-			// until Consul marks the service instance unavailable for mesh traffic, causing the connection to fail.
-			// We are expecting a "connection reset by peer" error because in a case of health checks,
-			// there will be no healthy proxy host to connect to. That's why we can't assert that we receive an empty reply
-			// from server, which is the case when a connection is unsuccessful due to intentions in other tests.
-			logger.Log(t, "checking that connection is unsuccessful")
-			k8s.CheckStaticServerConnectionMultipleFailureMessages(
-				t,
-				ctx.KubectlOptions(t),
-				false,
-				staticClientName,
-				[]string{"curl: (56) Recv failure: Connection reset by peer", "curl: (52) Empty reply from server"},
-				"http://localhost:1234")
-		})
+				logger.Log(t, "checking that connection is successful")
+				if tproxyEnabled {
+					k8s.CheckStaticServerConnectionSuccessful(t, ctx.KubectlOptions(t), staticClientName, "http://static-server")
+				} else {
+					k8s.CheckStaticServerConnectionSuccessful(t, ctx.KubectlOptions(t), staticClientName, "http://localhost:1234")
+				}
+
+				// Test that kubernetes readiness status is synced to Consul.
+				// Create the file so that the readiness probe of the static-server pod fails.
+				logger.Log(t, "testing k8s -> consul health checks sync by making the static-server unhealthy")
+				k8s.RunKubectl(t, ctx.KubectlOptions(t), "exec", "deploy/"+staticServerName, "--", "touch", "/tmp/unhealthy")
+
+				// The readiness probe should take a moment to be reflected in Consul, CheckStaticServerConnection will retry
+				// until Consul marks the service instance unavailable for mesh traffic, causing the connection to fail.
+				// We are expecting a "connection reset by peer" error because in a case of health checks,
+				// there will be no healthy proxy host to connect to. That's why we can't assert that we receive an empty reply
+				// from server, which is the case when a connection is unsuccessful due to intentions in other tests.
+				logger.Log(t, "checking that connection is unsuccessful")
+				if tproxyEnabled {
+					k8s.CheckStaticServerConnectionMultipleFailureMessages(
+						t,
+						ctx.KubectlOptions(t),
+						false,
+						staticClientName,
+						[]string{"curl: (56) Recv failure: Connection reset by peer", "curl: (52) Empty reply from server"},
+						"http://static-server")
+				} else {
+					k8s.CheckStaticServerConnectionMultipleFailureMessages(
+						t,
+						ctx.KubectlOptions(t),
+						false,
+						staticClientName,
+						[]string{"curl: (56) Recv failure: Connection reset by peer", "curl: (52) Empty reply from server"},
+						"http://localhost:1234")
+				}
+
+			})
+		}
 	}
 }
 
@@ -111,6 +146,7 @@ func TestConnectInject_CleanupKilledPods(t *testing.T) {
 			ctx := suite.Environment().DefaultContext(t)
 
 			helmValues := map[string]string{
+				"global.imageK8S":              "kschoche/consul-k8s-dev2",
 				"connectInject.enabled":        "true",
 				"global.tls.enabled":           strconv.FormatBool(c.secure),
 				"global.tls.enableAutoEncrypt": strconv.FormatBool(c.autoEncrypt),
