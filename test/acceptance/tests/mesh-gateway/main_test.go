ef5654e65876d2859179a423810162fd632e8515
Iryna Shustava <iryna@hashicorp.com>
2021-05-04 17:07:26 -0600

    Run transparent proxy tests as a parallel job and enable tproxy on nightly

--- test/acceptance/tests/mesh-gateway/main_test.go
+++ test/acceptance/tests/mesh-gateway/main_test.go
@@ -13,7 +13,8 @@ var suite testsuite.Suite
 func TestMain(m *testing.M) {
 	suite = testsuite.NewSuite(m)
 
-	if suite.Config().EnableMultiCluster {
+	// We don't want to run these tests when transparent proxy is enabled since multi-cluster is not yet supported.
+	if suite.Config().EnableMultiCluster && !suite.Config().EnableTransparentProxy {
 		os.Exit(suite.Run())
 	} else {
 		fmt.Println("Skipping mesh gateway tests because -enable-multi-cluster is not set")
